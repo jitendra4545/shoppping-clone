782558a87101f2db24e78f2468aa82339dfbb2a0
jitendra4545 <jitendra45.js45@gmail.com>
2023-05-14 01:19:30 +0530

    added

---
+++ mini-shopping-app/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <Box>
+        <Box>
+            
+        </Box>
+    </Box>
+  )
+}
---
+++ mini-shopping-app/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+
+export const HomePage = () => {
+  return (
+    <Box>
+        <Navbar/>
+        HomePage
+        </Box>
+  )
+}
