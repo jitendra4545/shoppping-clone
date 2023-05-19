11cd0182b8b37b0f892ded0b853cd642da3a66a6
jitendra4545 <jitendra45.js45@gmail.com>
2023-05-19 08:48:31 +0530

    added

--- mini-shopping-app/src/components/Navbar.jsx
+++ mini-shopping-app/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import { Box, Button, Image, Img, Input, Text,Popover,PopoverTrigger,PopoverContent,PopoverHeader,PopoverArrow,PopoverCloseButton,PopoverFooter,PopoverBody,ButtonGroup } from '@chakra-ui/react'
+import { Box, Button, Image, Img, Input, Text,Popover,PopoverTrigger,PopoverContent,PopoverHeader,PopoverArrow,PopoverCloseButton,PopoverFooter,PopoverBody,ButtonGroup, Tabs, TabList, Tab } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import {CgProfile} from 'react-icons/cg'
 import {ImHeart} from 'react-icons/im'
 import {BsSearch,BsCart4} from 'react-icons/bs'
 import { useSelector } from 'react-redux'
-//import { useDisclosure } from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/react'
+
 const link=[
   {"name":"MENS"},
   {"name":"WOMENS"},
@@ -19,29 +20,28 @@ const link=[
 export const Navbar = () => {
   const initialFocusRef = React.useRef()
 const [Auth, setAuth] = useState(false)
-//   const is_Active=useSelector(store=>store.AuthReducer)
+const [isSmall] = useMediaQuery('(min-width: 1430px)')
 
-// console.log(is_Active)
-  
   return (
     <Box bg='teal.100' p="5px 10px"  h='70px' boxShadow={'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;'} > 
       <Box display={'flex'} justifyContent={'center'} alignItems={'center'} >
       <Box>
-        <Image borderRadius={'30px'} w='60px' src='https://i.postimg.cc/BbcZrkVF/Pink-and-Black-Modern-Initials-Logo-Design.png'  />
+        <Image borderRadius={'30px'} w={{base:"30px",md:"40px",lg:"60px"}} src='https://i.postimg.cc/BbcZrkVF/Pink-and-Black-Modern-Initials-Logo-Design.png'  />
       </Box>
-<Box>
-
+<Tabs>
+<TabList>
 
       {
         link.map((el)=>{
-return <Button bg='teal.100' _hover={{'bg':"blue",color:"white"}} ml={'8'} key={el.name} >
+return <Tab bg='teal.100' _hover={{'bg':"blue",color:"white"}} ml={{base:0,md:0,lg:8}} key={el.name} >
          {el.name}  
-            </Button>
+            </Tab>
         })
       }
-</Box>
+      </TabList>
+</Tabs>
       <Box m="0px 30px" display={'flex'}>
-            <Input placeholder='Search for products , brands and more ...' mr='5px' border={'3px solid grey'} w='20rem' />
+            <Input placeholder='Search for products , brands and more ...' mr='5px' border={'3px solid grey'} w={{base:"15rem",md:"16rem",lg:"20rem"}} />
 <Button bg='teal.100' _hover={{'bg':"blue",color:"white"}}><BsSearch size={'20px'} /></Button>
       </Box>
       <Box>
@@ -83,15 +83,32 @@ return <Button bg='teal.100' _hover={{'bg':"blue",color:"white"}} ml={'8'} key={
       </PopoverTrigger>
       <PopoverContent color='white' bg='blue.800' borderColor='blue.800'>
         <PopoverHeader pt={4} fontWeight='bold' border='0'>
-         WELCOME
+         WELCOME name
+         
+            
+
         </PopoverHeader>
         <PopoverArrow />
         <PopoverCloseButton />
         <PopoverBody>
           <Box>
-            <Text>
-                  
+            
+          <Image ml='70px' w='50%' src='https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp' />
+            <Text  fontWeight={'bold'} mt='10px' >
+                 My Account
             </Text>
+            <Text  fontWeight={'bold'} mt='10px' >
+                 My Orders
+                  </Text>
+                  <Text  fontWeight={'bold'} mt='10px' >
+                     Login As Admin                  
+                  </Text>
+                  <Text>
+                  
+                  </Text>
+                  <Text>
+                  
+                  </Text>
           </Box>
         </PopoverBody>
         <PopoverFooter
@@ -102,12 +119,10 @@ return <Button bg='teal.100' _hover={{'bg':"blue",color:"white"}} ml={'8'} key={
           pb={4}
         >
           
-          <ButtonGroup size='sm'>
-            <Button colorScheme='green'>Setup Email</Button>
-            <Button colorScheme='blue' ref={initialFocusRef}>
-              Next
-            </Button>
-          </ButtonGroup>
+          
+            <Button colorScheme='green'>LogOut</Button>
+           
+        
         </PopoverFooter>
       </PopoverContent>
     </Popover>
