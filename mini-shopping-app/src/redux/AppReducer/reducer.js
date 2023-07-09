fd3a140c5ec0a9cc6ae25c5cffe5f65e2872377f
jitendra4545 <jitendra45.js45@gmail.com>
2023-07-09 22:52:49 +0530

    added

---
+++ mini-shopping-app/src/redux/AppReducer/reducer.js
@@ -0,0 +1,17 @@
+let initialData={
+    count:0
+}
+
+
+export const reducer=(state=initialData,action)=>{
+
+    const {type,payload}=action
+
+    switch(type){
+
+        default :
+        return state
+    }
+
+
+}
